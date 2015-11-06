23600e698485870655b0967f679fb7ce3f3b8891
yangyang01011001 <yangyang01011001@gmail.com>
2015-11-06 14:51:52 -0600

    Update and rename function.sql to function_and_procedure.sql

--- function_and_procedure.sql
+++ function_and_procedure.sql
@@ -51,3 +51,50 @@ begin
 	end if;
 end;
 /
+
+create or replace procedure check_q(qnum NUMBER)
+as 
+	nexcess integer;
+	nmissing integer;
+	q varchar(4);
+	q_expected varchar(13);
+	query1 varchar(100);
+	query2 varchar(100);
+	marked number;
+begin
+--	q :=UPPER(qnum);
+--	q_expected := UPPER(qnum)||'_EXPECTED';
+	marked := 0;
+	query1 := 'select count(*) from ((select * from Q'||qnum||') MINUS (select * from Q'||qnum||'_EXPECTED))';
+	query2 := 'select count(*) from ((select * from Q'||qnum||'_EXPECTED) MINUS (select * from Q'||qnum||'))';
+	if (not ass2_view_exists('Q'||qnum)) then
+		marked:=1;
+		DBMS_OUTPUT.PUT_LINE('No Q'||qnum||' View; did it load correctly?');
+	elsif (not ass2_table_exists('Q'||qnum||'_EXPECTED')) then
+		DBMS_OUTPUT.PUT_LINE('Q_'||qnum||': No expected results! run expected.sql!');
+		marked:=1;
+	else
+		execute immediate query1 into nexcess;
+		execute immediate query2 into nmissing;
+		DBMS_OUTPUT.PUT_LINE('Q'||qnum|| ass2_check_result(q,nexcess,nmissing));
+		marked:=1;
+	end if;
+	if(marked=0) then
+		DBMS_OUTPUT.PUT_LINE('???');
+	end if;
+end;
+/
+
+
+create or replace procedure check_all(qnum number)
+as
+begin
+	for i in 1..qnum loop
+		check_q(i);
+	end loop;
+end;
+/
+
+
+set serveroutput on
+
